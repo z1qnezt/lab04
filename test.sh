8a4d83da707185296e87b925e4d9251562293086
z1qnezt <misha232dog@gmail.com>
2024-04-30 19:27:26 +0300

    YRAAAAAAAA

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ cmake --build $root/hello_world_application
 cmake --build $root/solver_lib
 cmake --build $root/solver_application
 
-$root/hello_world_application/_build/main
-echo -e '1\n2\n3' | $root/solver_application/_build/main
+$root/hello_world_application/_build/hello_world
+echo -e '1\n2\n3' | $root/solver_application/_build/solver
